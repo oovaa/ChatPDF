6f5fd0619aa939b000eea73e4ccb3a2fe0146b5f
oovaa <omarvx211@gmail.com>
2024-04-17 15:09:12 +0200

    ive created a small gemini CLI at 17-15

--- gemini.js
+++ gemini.js
@@ -1,24 +1,44 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import detenv from "dotenv";
+import readline from "readline"
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 detenv.config()
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 async function run() {
 
     const model = genAI.getGenerativeModel({ model: "gemini-pro" })
 
-    const prompt = "text-embedding-001 which is free ?"
 
-    const re = await model.generateContent(prompt)
-    const ans = re.response;
+    const chat = model.startChat({
+        history: [],
+        generationConfig: {
+            maxOutputTokens: 500,
+        },
+    })
 
-    const text = ans.text();
-
-    console.log(text);
+    async function ask_respond() {
+        rl.question("You: ", async (msg) => {
+            if (msg.toLowerCase === "exit")
+                rl.close()
+            else {
+                const result = await chat.sendMessage(msg)
+                const respond = result.response
+                // console.log("Respond object:", respond);
+                const text = respond.text()
+                console.log("AI: ", text);
+                ask_respond()
+            }
+        })
+    }
+    ask_respond()
 }
 
 run()
