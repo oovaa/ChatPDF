a82db4149f45e8d382cf3ef5a88bf54eca40dba9
oovaa <omarvx211@gmail.com>
2024-04-17 16:11:17 +0200

    a streaming version with an error at line 37 at 17-16

---
+++ gemini_streaming.js
@@ -0,0 +1,64 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+import readline from "readline"
+// Access your API key as an environment variable (see "Set up your API key" above)
+dotenv.config()
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let isWaitingRespond = false
+
+async function run() {
+
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+
+    const chat = model.startChat({
+        history: [],
+        generationConfig: {
+            maxOutputTokens: 500,
+        },
+    })
+
+    async function ask_respond() {
+        if (!isWaitingRespond) {
+            rl.question("You: ", async (msg) => {
+                if (msg.toLocaleLowerCase() === "exit")
+                    rl.close()
+
+                else {
+                    console.log("in the else");
+                    isWaitingRespond = true
+                    try {
+                        const result = await chat.sendMessageStream(msg)
+                        console.log("in the try");
+                        let text = ""
+                        console.log(result);
+                        for await (const chunk of result.stream) {
+                            console.log("in the for");
+
+                            const chunk_text = chunk.text()
+                            console.log("AI: ", chunk_text);
+                            text += chunk_text
+                        }
+                        isWaitingRespond = false
+                        ask_respond()
+                    } catch (error) {
+                        console.error("ERROR: ", error)
+                        isWaitingRespond = false
+
+                    }
+
+                }
+            })
+
+        }
+    }
+    ask_respond()
+}
+
+run()
