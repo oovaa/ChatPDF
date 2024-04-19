3f72d59c692d6edb9c0e6af9231b9f74e713ee33
iiihassanii <hassanabdelazim.max@gmail.com>
2024-04-19 18:15:40 +0300

    setting up The route methods
    doing post chat route
    doing gat chat route
    doing delete chat route

--- app/index.js
+++ app/index.js
@@ -1,44 +1,93 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-// ! To check if the file exists, may remove it later
-// AIzaSyCTw6-HUUp_YNPKMe4siWSC9GZoH_knmB4
-const fs = require('fs');
+
 
 const port = 5000
 
+//! pdf sample test list , Delete later
+const pdfs = [
+    { id: 1, name: 'example1.pdf' },
+    { id: 2, name: 'example2.pdf' },
+    { id: 3, name: 'example3.pdf' }
+];
+
+const chats = [
+    { id: 1, name: 'chat1' },
+    { id: 2, name: 'chat2' },
+    { id: 3, name: 'chat3' }
+];
 
 app.use(express.json());
 
-// * /*`/api/pdfs`*/
+// * start of `/api/pdfs`*/
 /*POST: Upload a PDF document*/
+// ? We may face some issues with this!
 app.post("/api/pdfs", (req, res) => {
-        res.sendFile(path.join(__dirname, 'pdf.txt'));
+    
 });
 
 /*Get all PDF documents for a user*/
 app.get("/api/pdfs", (req, res) => {
-        const filePath = path.join(__dirname, '/pdf.txt');
-        fs.access(filePath, fs.constants.F_OK, (err) => {
-            if (err) {
-                res.status(404).send("File not found");
-            } else {
-                res.sendFile(filePath);
-            }
-        });
-    });
+    
+    if (!pdfs)
+    {
+        res.status(404).send("PDFS not found");
+    } else
+    {
+        res.send(pdfs);
+    }
+    
+});
 
+/*DELETE: Delete a PDF document*/
+app.delete("/api/pdfs/:id", (req, res) => {
+    const id = req.params.id * 1;
+    const pdfToDelete = pdfs.find(el => el.id === id);
+    const index = pdfs.indexOf(pdfToDelete);
+    pdfs.splice(index, 1);
 
+    res.send(pdfs);
+    // test with "curl -X DELETE http://localhost:5000/api/pdfs/2"
+});
 
-// *  /*`/api/chats`*/
-/*Get all chats for a user*/
+// *  Start of `/api/chats`
+
+/*POST: Create a new chat*/
+app.post("/api/chats", (req,res) => {
+    const newId = chats.length + 1;
+    const newChat = Object.assign({ id: newId }, req.body);
+    chats.push(newChat);
+    res.status(201).json({ message: "New chat has been created", chat: newChat });
+    // test with "curl -X POST http://localhost:5000/api/chats -H "Content-Type: application/json" -d '{"name": "chat4"}' -vvv"
+
+})
+/*GET: Get all chats for a user*/
 app.get("/api/chats", (req, res) => {
-        res.sendFile(path.join(__dirname, 'pdf.txt'));
+    
+    if (!chats)
+    {
+        res.status(404).send("PDFS not found");
+    } else
+    {
+        res.send(chats);
+    }
+    
+});
+/*DELETE: Delete a chat*/
+app.delete("/api/chats/:id", (req, res) => {
+    const id = req.params.id * 1;
+    const chatToDelete = chats.find(el => el.id === id);
+    const index = chats.indexOf(chatToDelete);
+    pdfs.splice(index, 1);
+
+    res.send(pdfs);
+    // test with "curl -X DELETE http://localhost:5000/api/chats/2"
 });
 
 
 
-// *  /*`/api/messages`*/
+// *  Start of `/api/messages`*/
 /*GET: Get all messages for a chat*/
 app.get("/api/messages", (req, res) => {
         res.sendFile(path.join(__dirname, 'pdf.txt'));
