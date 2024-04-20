ea2ecb7f6172d44cbf8a5a271f50509f7a1adb81
oovaa <omarvx211@gmail.com>
2024-04-20 15:16:43 +0200

    file chunker works

---
+++ tools/chuncker.js
@@ -0,0 +1,33 @@
+import { TextLoader } from "langchain/document_loaders/fs/text"
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+TextLoader
+
+
+async function file_chuncker(path) {
+    try {
+
+        const loader = new TextLoader(path)
+        const file = await loader.load()
+
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkOverlap: 0,
+            chunkSize: 700
+        })
+        const out = await splitter.splitDocuments(file)
+        return out
+
+    } catch (error) {
+        console.log("chuncker problem ", error);
+    }
+}
+
+
+
+export { file_chuncker };
+
+// uncomment to test
+
+// const re = await file_chuncker("playing_around/scrimba.txt")
+
+// console.log(re);
+
