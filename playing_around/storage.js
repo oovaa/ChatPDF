02e171e6e5f554a738997ecb388d1296082902df
oovaa <omarvx211@gmail.com>
2024-04-20 15:45:18 +0200

    the storage is working next step is to search from the db

--- playing_around/storage.js
+++ playing_around/storage.js
@@ -1,14 +1,12 @@
 import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
-import { TextLoader } from "langchain/document_loaders/fs/text";
-import { file_juncker } from "../tools/juncker";
+import { file_chuncker } from "../tools/chuncker";
 
 
 // Create docs with a loader
-const loader = new TextLoader("./playing_around/scrimba.txt");
-const file = await loader.load();
+const path = "./playing_around/scrimba.txt";
 
-const docs = await file_juncker(file.toString())
+const docs = await file_chuncker(path)
 
 
 // Load the docs into the vector store
@@ -16,9 +14,6 @@ const vectorStore = await HNSWLib.fromDocuments(docs, new GoogleGenerativeAIEmbe
     model: "embedding-001"
 }));
 
-// Search for the most similar document
-const result = await vectorStore.similaritySearch("omar", 1);
-console.log(result);
 
 const directory = "playing_around/db";
 await vectorStore.save(directory);
