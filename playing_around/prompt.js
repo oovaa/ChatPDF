db7d2b4c78c63268cce9cd4e31cf8d28d6ac2662
oovaa <omarvx211@gmail.com>
2024-04-24 23:12:25 +0200

    generate a stand alone question

--- playing_around/prompt.js
+++ playing_around/prompt.js
@@ -4,14 +4,15 @@ import { Cgoogle, Chat_google } from '../models/Cmodels';
 const llm = Chat_google();
 
 const tweet_template =
-  'generate a promotinal tweet for a product, from this product discription: {productDesc}';
+  'given a question generatea a stand alone question: {question}';
 
 const tweet = PromptTemplate.fromTemplate(tweet_template);
 
 const tweet_chain = tweet.pipe(llm);
 
 const response = await tweet_chain.invoke({
-  productDesc: 'dell laptop'
+  question:
+    'If you were to think about European capitals, which city would come to mind when considering the country known for its Eiffel Tower and fine wine?'
 });
 
 console.log(response.content);
