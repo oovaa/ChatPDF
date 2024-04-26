8de974964ba65e5ec75b85e3281eaf95d8ac18c8
oovaa <omarvx211@gmail.com>
2024-04-26 06:34:43 +0200

    Refactor langchain.js to include answer template and combine function

--- playing_around/langchain.js
+++ playing_around/langchain.js
@@ -1,12 +1,20 @@
 import { PromptTemplate } from '@langchain/core/prompts';
 import { Chat_google } from '../models/Cmodels';
 import { StringOutputParser } from 'langchain/schema/output_parser';
-import { retrevire } from './retriver';
+import { combine, retrevire } from './retriver';
 
 const llm = Chat_google();
 
 const tweet_template =
   'given a question generate a stand alone question: {question} standalone question:';
+
+const ans_template = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the provided context.
+   If you don't know the answer, say 'I'm sorry, I don't know the answer to that' and direct the question to email help@scrimba.com.
+    Please avoid making up an answer. Always speak as if you were chatting with a friend.
+    context: {context}
+    question: {question}
+    answer:
+    `;
 
 const tweet_prompt = PromptTemplate.fromTemplate(tweet_template);
 
@@ -14,9 +22,8 @@ const chain = tweet_prompt
   .pipe(llm)
   // @ts-ignore
   .pipe(new StringOutputParser())
-  .pipe(retrevire);
-
-console.log(chain);
+  .pipe(retrevire)
+  .pipe(combine);
 
 const response = await chain.invoke({
   question: "I'm a complete beginner adn really nervous. Is scrimba for me?"
