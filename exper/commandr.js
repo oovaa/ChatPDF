b78a5f5e1211096e75822ae91a9a757b66dd3120
oovaa <omarvx211@gmail.com>
2024-05-07 11:09:50 +0200

    the MF now remmembers

--- exper/commandr.js
+++ exper/commandr.js
@@ -1,16 +1,13 @@
 import { ChatCohere } from '@langchain/cohere';
-import { ChatPromptTemplate, isLLM, PromptTemplate } from 'langchain/prompts';
+import { PromptTemplate } from 'langchain/prompts';
 import { StringOutputParser } from 'langchain/schema/output_parser';
 import { RunnableSequence } from 'langchain/schema/runnable';
 import { createInterface } from 'readline';
-import { retriever } from '../tools/retriver';
+import { formatConv } from './conv_history';
 
 const EXIT_COMMAND = 'exit';
 const RESPONSE_COLOR = '\x1b[32m%s\x1b[0m'; // Green
 
-const prompt = ChatPromptTemplate.fromMessages([
-  ['human', 'Tell me a short joke about {topic}']
-]);
 const llm = new ChatCohere({ model: 'command-r-plus' });
 
 const grammer_template = `Given a question, fix the grammar problems if any.
@@ -26,6 +23,13 @@ standalone question:`;
 
 const stand_alone_prompt = PromptTemplate.fromTemplate(stand_alone_template);
 
+const answer_template = `Given a question, provide an answer from the history if available, or generate a new answer.
+Question: {question}
+History: {history}
+Answer:
+`;
+const answer_prompt = PromptTemplate.fromTemplate(answer_template);
+
 // @ts-ignore
 const grammer_chain = RunnableSequence.from([
   grammer_prompt,
@@ -37,6 +41,13 @@ const grammer_chain = RunnableSequence.from([
 // @ts-ignore
 const stand_alone_chain = RunnableSequence.from([
   stand_alone_prompt,
+  llm,
+  new StringOutputParser()
+]);
+
+// @ts-ignore
+const answer_chain = RunnableSequence.from([
+  answer_prompt,
   llm,
   new StringOutputParser()
 ]);
@@ -58,6 +69,7 @@ const chain = RunnableSequence.from([
   stand_alone_chain
   // (prevResult) => console.log(prevResult),
 ]);
+const history = [];
 
 async function ask() {
   const question = await new Promise((resolve) =>
@@ -66,11 +78,14 @@ async function ask() {
   if (question.toLocaleLowerCase() === EXIT_COMMAND) {
     rl.close();
   } else {
-    const response = await chain.invoke({
-      question: question
+    const response = await answer_chain.invoke({
+      question: question,
+      // @ts-ignore
+      history: formatConv(history)
     });
+    history.push(question);
+    history.push(response);
     console.log(RESPONSE_COLOR, response);
-
     ask();
   }
 }
