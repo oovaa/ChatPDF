242c563350d030d8397a19d09ac6c4a08c5ec0dd
iiihassanii <hassanabdelazim.max@gmail.com>
2024-05-10 13:17:41 +0300

    handle 404 page route clear the code

--- app/index.js
+++ app/index.js
@@ -22,6 +22,15 @@ files from a directory named "public" - app/public
 app.use(express.static(new URL('public', import.meta.url).pathname));
 app.use(bodyParser.json());
 
+
+/**
+ * Handles a POST request to '/send-message' endpoint, expecting a message in the request body.
+ * Sends the message for processing and responds with the result.
+ * 
+ * @param {Object} req - The request object containing the message to be sent.
+ * @param {Object} res - The response object to send the result.
+ * @returns {Promise<void>} - A promise indicating the completion of the handling process.
+ */
 app.post('/send-message', async (req, res) => {
   const message = req.body.message;
   try {
@@ -36,29 +45,22 @@ app.post('/send-message', async (req, res) => {
   }
 });
 
-/* Multer disk storage configuration to
-create uniqe file name and save it into './app/PDFfiles */
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const destinationPath =
-      dirname(fileURLToPath(import.meta.url)) + '/PDFfiles';
-    cb(null, destinationPath);
-  },
-  filename: (req, file, cb) => {
-    const uniqueFileName = uuidv4(); // Generate a unique file name
-    const fileExtension = file.originalname.split('.').pop(); // Get the file extension
-    const fileName = `${uniqueFileName}.${fileExtension}`; // Combine unique name and extension
-    cb(null, fileName);
-  }
-});
-const upload = multer({ storage: storage });
 
-app.post('/chat', upload.single('file'), async (req, res) => {
-  try {
-    const fileName = req.file.filename;
-    const filePath = `${req.file.destination}/${fileName}`;
-
-    const doc = await load_pdf(filePath);
+/**
+ * Asynchronously handles a PDF file, processing its contents:
+ * 1. Loads the PDF document from the specified file path.
+ * 2. Divides the document into chunks for further processing.
+ * 3. Embeds the chunks using ECohereEmbeddings and saves the output into the "app/db" directory.
+ * 4. Deletes the original PDF file.
+ * 5. Loads the vector database from the saved directory.
+ * 6. Initiates a retrieval process.
+ * 7. Combines the processed chunks.
+ * 
+ * @param {string} filePath - The path to the PDF file to be handled.
+ * @returns {Promise<void>} - A promise indicating the completion of the handling process.
+ */
+async function processPDF(filePath){
+  const doc = await load_pdf(filePath);
     const chuncks = await doc_chuncker(doc);
 
     // embedding and save the output into "app/db"
@@ -80,6 +82,39 @@ app.post('/chat', upload.single('file'), async (req, res) => {
     const load_vectore = await H_load_vectore(targetDir, ECohereEmbeddings);
     await retriever;
     combine(chuncks);
+}
+/* Multer disk storage configuration to
+create uniqe file name and save it into './app/PDFfiles */
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const destinationPath =
+      dirname(fileURLToPath(import.meta.url)) + '/PDFfiles';
+    cb(null, destinationPath);
+  },
+  filename: (req, file, cb) => {
+    const uniqueFileName = uuidv4(); // Generate a unique file name
+    const fileExtension = file.originalname.split('.').pop(); // Get the file extension
+    const fileName = `${uniqueFileName}.${fileExtension}`; // Combine unique name and extension
+    cb(null, fileName);
+  }
+});
+const upload = multer({ storage: storage });
+
+/**
+ * Handles a POST request to '/chat' endpoint, which expects a single file upload.
+ * Upon receiving the file, it processes the PDF content asynchronously.
+ * 
+ * @param {Object} req - The request object containing file information.
+ * @param {Object} res - The response object to send the result.
+ * @returns {Promise<void>} - A promise indicating the completion of the handling process.
+ */
+app.post('/chat', upload.single('file'), async (req, res) => {
+  try {
+    const fileName = req.file.filename;
+    const filePath = `${req.file.destination}/${fileName}`;
+
+    processPDF(filePath)
+    
 
     res.send('ok');
   } catch (error) {
@@ -93,6 +128,10 @@ app.post('/chat', upload.single('file'), async (req, res) => {
 });
 
 
+/*404 page route */
+  app.get('*', (req, res) => {
+    res.sendFile(new URL('public/404.html', import.meta.url).pathname)
+  })
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}
