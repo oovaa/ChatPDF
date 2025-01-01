854c93ab815d5e936875467c888708570e3f7797
oovaa <omarvx211@gmail.com>
2025-01-01 15:11:58 +0200

    add document chunking utility and refactor file processing

---
+++ chatpdf/src/utils/chunker.js
@@ -0,0 +1,20 @@
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'
+
+/**
+ * This function takes a document as input and splits it into chunks.
+ * Each chunk is of size 700 characters and there is no overlap between chunks.
+ * @param {Document[]} doc - The document to be chunked.
+ * @returns {Promise<any[]>} - A promise that resolves to an array of chunks.
+ */
+export async function doc_chuncker(doc) {
+  try {
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkOverlap: 0,
+      chunkSize: 700,
+    })
+    const out = await splitter.splitDocuments(doc)
+    return out
+  } catch (error) {
+    console.log('chuncker problem ', error)
+  }
+}
--- chatpdf/src/utils/fileProcessing.js
+++ chatpdf/src/utils/fileProcessing.js
@@ -1,4 +1,3 @@
-import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'
 import { TextLoader } from 'langchain/document_loaders/fs/text'
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
 import { DocxLoader } from '@langchain/community/document_loaders/fs/docx'
@@ -58,22 +57,4 @@ export const parser = async (filePath) => {
   }
 }
 
-/**
- * This function takes a document as input and splits it into chunks.
- * Each chunk is of size 700 characters and there is no overlap between chunks.
- * @param {Document[]} doc - The document to be chunked.
- * @returns {Promise<any[]>} - A promise that resolves to an array of chunks.
- */
-export async function doc_chuncker(doc) {
-  try {
-    const splitter = new RecursiveCharacterTextSplitter({
-      chunkOverlap: 0,
-      chunkSize: 700,
-    })
-    const out = await splitter.splitDocuments(doc)
-    return out
-  } catch (error) {
-    console.log('chuncker problem ', error)
-  }
-}
 
