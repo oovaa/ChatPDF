686ad37fa3b5aeeb390b525a8bfab3c7511619d8
oovaa <omarvx211@gmail.com>
2025-01-03 22:10:40 +0200

    adding multer middleware and moving routes to a saperate dir

--- chatpdf/index.js
+++ chatpdf/index.js
@@ -1,18 +1,16 @@
 import express, { json } from 'express'
 import logger from './src/middleware/logger.js'
+import { router } from './src/Routes/index.js'
 
 const app = express()
-const port = 3000
+const PORT = 3000
 
 app.use(express.json())
 app.use(logger)
+app.use(router)
 
-app.get('/', (req, res) => res.status(200).send('all good'))
-app.post('/', (req, res) => {
-  res.status(200).send('all good ')
-})
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}
   access it with the link http://localhost:3000/`)
 })
