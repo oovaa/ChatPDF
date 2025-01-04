38ea7669028d206823f90c5036b8d0655d80cf96
oovaa <omarvx211@gmail.com>
2025-01-04 10:59:51 +0200

    Refactor routes to improve error handling and update endpoint structure

--- chatpdf/src/Routes/index.js
+++ chatpdf/src/Routes/index.js
@@ -1,7 +1,6 @@
 import tempWrite from 'temp-write'
 import { Router } from 'express'
 import upload from '../middleware/multerMiddleWare'
-import { parser } from '../utils/fileProcessing.js'
 import { StoreFileInVDB } from '../db/hnsw.js'
 import { setVDB } from '../db/retriver.js'
 import { chain } from '../utils/chains.js'
@@ -9,19 +8,15 @@ import { chain } from '../utils/chains.js'
 export const router = Router()
 let history = ''
 
-router.get('/', (req, res) => {
+router.get('/z', (req, res) => {
   res.status(200).send('all good')
-})
-router.post('/', (req, res) => {
-  res.status(200).send('all good ')
 })
 
 router.post('/send', async (req, res) => {
   const question = req.body.question
-  let answer
 
   try {
-    answer = await chain.invoke({
+    const answer = await chain.invoke({
       question,
       history,
     })
@@ -31,16 +26,30 @@ router.post('/send', async (req, res) => {
     res.status(200).json({ answer })
   } catch (error) {
     console.error(error)
-    res
-      .status(500)
-      .json({ error: 'An error occurred while processing your request.' })
+    res.status(500).json({
+      error: `An error occurred while processing your request: ${error.message}`,
+    })
   }
 })
 
 router.post('/upload', upload, async (req, res, next) => {
-  const filePath = tempWrite.sync(req.file.buffer, req.file.originalname)
-  const vdb = await StoreFileInVDB(filePath)
-  setVDB(vdb)
+  try {
+    if (!req.file) throw new Error('no file uploaded')
+
+    const filePath = tempWrite.sync(req.file.buffer, req.file.originalname)
+    const vdb = await StoreFileInVDB(filePath)
+    setVDB(vdb)
+    console.log('file stored in the vector db')
+  } catch (error) {
+    console.error(error)
+    return res
+      .status(500)
+      .json({
+        error: `An error occurred while uploading the file: ${
+          (error.message)
+        }`,
+      })
+  }
 
   res.status(200).send('file stored in the vector db')
 })
