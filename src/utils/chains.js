9642d444003c330079944daaa7c0b9fb10d66385
oovaa <omarvx211@gmail.com>
2025-01-09 13:03:58 +0200

    Refactor response templates in chains.js for clarity and consistency; add markdown format instruction

--- src/utils/chains.js
+++ src/utils/chains.js
@@ -32,6 +32,8 @@ Instructions for answering:
 - For simple general knowledge outside the context, answer naturally
 - For complex topics outside the provided context, inform the user with "Note: This answer is based on external knowledge."
 - Always maintain a friendly tone
+- Use markdown format
+
 
 Context:
 {context}
@@ -47,21 +49,21 @@ Answer:
 
 /**
  * Template string for generating responses in ChatPDF.
- * 
+ *
  * This template is used to create a response for the user based on the assistant's knowledge and history of previous interactions.
- * 
+ *
  * Instructions for answering:
  * - If the answer is found in the history, provide it directly.
  * - Always maintain a friendly tone.
- * 
+ *
  * Variables:
  * - {history}: The history of previous interactions.
  * - {question}: The current question asked by the user.
- * 
+ *
  * The assistant should never mention that it is a computer program, robot, virtual assistant, or AI Assistant.
  */
 const no_Doc_template = `
-Your name is ChatPDF Created by Omar Mohayyad and Hassan, you are a helpful AI assistant. Answer the user's question based on the your Knowledge with clear and accurate details.
+Your name is ChatPDF Created by Omar Mohayyad and Hassan, you are a helpful AI assistant. Answer the user's question with clear and accurate details.
 # NEVER mention that you are a computer program, robot, virtual assistant or AI Assistant.
 
 Answer user questions kindly using Your knowledge
@@ -69,6 +71,7 @@ Answer user questions kindly using Your knowledge
 Instructions for answering:
 - If the answer is found in the history, provide it directly
 - Always maintain a friendly tone
+- Use markdown format
 
 
 History:
@@ -95,7 +98,7 @@ const ans_prompt = PromptTemplate.fromTemplate(ans_template)
 /**
  * A prompt template created from the no_Doc_template.
  * This template is used for generating prompts when no document is available.
- * 
+ *
  * @constant {PromptTemplate} no_doc_chain_prompt
  */
 const no_doc_chain_prompt = PromptTemplate.fromTemplate(no_Doc_template)
@@ -131,9 +134,9 @@ export const answer_chain = RunnableSequence.from([
 ])
 
 /**
- * A sequence of runnable tasks that includes a prompt, a language model, 
+ * A sequence of runnable tasks that includes a prompt, a language model,
  * and a string output parser. This sequence is used when no document is provided.
- * 
+ *
  * @constant {RunnableSequence} no_doc_chain - The sequence of tasks to run.
  */
 export const no_doc_chain = RunnableSequence.from([
